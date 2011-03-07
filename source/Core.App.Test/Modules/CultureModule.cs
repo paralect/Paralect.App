79b29c8642bd8f59b57b545288f90144edced184
Rationalle <rationalle@gmail.com>
2011-03-07 22:58:02 +0200

    New image

---
+++ source/Core.App.Test/Modules/CultureModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Core.App.Test.Modules
+{
+    public class CultureModule : AppModule
+    {
+        public override void Start()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        }
+
+        public override void Stop()
+        {
+            // No need for
+        }
+    }
+}
---
+++ source/Core.App.Test/Modules/MyApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.App.Test.Modules
+{
+    public class MyApp : App
+    {
+        protected override void Setup()
+        {
+            AddModule<TrackerModule1>();
+            AddModule<TrackerModule2>();
+            AddModule<TrackerModule3>();
+            AddModule<TrackerModule3>();            
+        }
+    }
+}
